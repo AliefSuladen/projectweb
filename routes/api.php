322df20735f80fe232d94f4cc06b9874e243116e
Alief <aliefsuladen56@gmail.com>
2022-11-17 00:43:15 +0700

    Create Api Data Event

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('event', [EventController::class, 'event']);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
